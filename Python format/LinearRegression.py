644c586d6ecf98e79d2ea8897978094910ce2d89
mouafak-khaled <mhasan18@ku.edu.tr>
2021-05-30 21:35:08 +0300

    added the required datasets

---
+++ Python format/LinearRegression.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class LinearRegression(Object):
+
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    def isValidData(self, X, Y):
+        # If the first parameter is not an array.
+        if not (isinstance(X, list) or isinstance(X, np.ndarray)):
+            raise Exception("Invalid input type. X is of type {}".format(type(X)))
+            return False
+
+        # If the second parameter is not an array.
+        if not (isinstance(Y, list) or isinstance(Y, np.ndarray)):
+            raise Exception("Invalid input type. Y is of type {}".format(type(Y)))
+            return False
+
+        # If the two inputs are of different shapes.
+        if isinstance(X, np.ndarray) and isinstance(Y, np.ndarray):
+            if X.shape != Y.shape:
+                raise Exception("Unmatched input shapes. X.shape = {}, Y.shape = {}".format(X.shape, Y.shape))
+                return False
+
+        elif isinstance(X, list) and isinstance(Y, list):
+            if len(X) != len(Y):
+                raise Exception(
+                    "Unmatched input lengths for X and Y. Length of X = {}, length of Y = {}".format(len(X), len(Y)))
+                return False
+
+        return True
+
+    
